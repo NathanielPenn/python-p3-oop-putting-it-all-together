24347f12c67623e45f492a3de4738ee69dea59d6
NatePennebaker <npenne1@aol.com>
2023-02-27 13:49:46 -0500

    completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class Shoe:
+
+    # constructor
+    def __init__(self, brand, size = 0):
+        self.brand = brand
+        self.size = size
+
+    def get_color(self):
+        return self.color
+    
+    def set_color(self, color):
+        self.color = color
+    
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if type (size) == int:
+            self._size = size
+        else:
+            print("Size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def get_material(self):
+        return self.material
+    
+    def get_condition(self):
+        return self.condition
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
     pass
